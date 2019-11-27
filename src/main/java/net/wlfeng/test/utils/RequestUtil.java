9a9cb095d33713bb55d44bac03692e0e6709763f
weilingfeng <weilingfeng@mageline.cn>
2019-11-27 18:02:46 +0800

    -添加拦截器打印请求响应信息

---
+++ src/main/java/net/wlfeng/test/utils/RequestUtil.java
@@ -0,0 +1,34 @@
+package net.wlfeng.test.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import net.wlfeng.test.filter.BodyReaderHttpServletRequestWrapper;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Map;
+
+/**
+ * @author weilingfeng
+ * @date 2019/11/2 12:00
+ * @description
+ */
+public class RequestUtil {
+
+    public static String getParams(BodyReaderHttpServletRequestWrapper request) throws IOException {
+        JSONObject result = new JSONObject();
+
+        if ("application/x-www-form-urlencoded".equals(request.getContentType())) {
+            Map map = request.getParameterMap();
+            for (Object key : map.keySet()) {
+                result.put(key.toString(), URLDecoder.decode(request.getParameter(key.toString()), "utf-8"));
+            }
+        }
+
+        if ("application/json".equals(request.getContentType())) {
+            result = JSON.parseObject(request.getParams());
+        }
+        return result.toJSONString();
+    }
+
+}
